f2d06acf5c05c59b05ea29874d660e9c0d9120e7
ESTOLEAR <stolearelena88@gmail.com>
2024-04-23 19:00:31 +0300

    homework lesson 4

--- src/homework_3/ex5.java
+++ src/homework_3/ex5.java
@@ -4,10 +4,12 @@ public class ex5 {
     public static void main(String[] args) {
        // 5.	Display on the screen the multiplication table for 3.
         int nr = 3;
+        int product;
         for(int i = 1; i <= 10; i++)
+
         {
-            //formula is copy because i don't know how to display like that(((
-            System.out.printf("%d * %d = %d \n", nr, i, nr * i);
+            product= nr*i;
+            System.out.printf(nr + "*"+i +"=" + product);
         }
     }
 }
